db3adb7189e2c2bd6c58b6ef3ed6b5f0bcb3942f
Imanolasolo <jjusturi@gmail.com>
2022-04-19 12:16:36 -0500

    100-puppet_ssh_config.pp:white_check_mark:

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,4 +1,4 @@
-# connect to a server Execute a command
+# connect to a server Execute a command.
 
 exec { 'echo "PasswordAuthentication no\nIdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config':
         path    => '/bin/'
