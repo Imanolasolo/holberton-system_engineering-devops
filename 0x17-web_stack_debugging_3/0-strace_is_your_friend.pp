50ae3d04d8cad60bed30bb13b433e7aa9f8f78e4
imanol <jjusturi@gmail.com>
2022-05-03 08:37:59 -0500

    0-strace_is_your_friend.pp :white_check_mark:

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,5 @@
 # Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
+# By: Imanol Asolo
 
 exec { 'fix-wordpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
